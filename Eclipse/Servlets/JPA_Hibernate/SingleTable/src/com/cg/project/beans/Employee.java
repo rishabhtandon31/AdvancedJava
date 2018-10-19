0605296bf1530c1243eb81776462e7b69a395f28
RISHABH <35215426+rishabhtandon31@users.noreply.github.com>
2018-10-19 20:37:02 +0530

    Add files via upload

---
+++ Eclipse/Servlets/JPA_Hibernate/SingleTable/src/com/cg/project/beans/Employee.java
@@ -0,0 +1,53 @@
+package com.cg.project.beans;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
+@Entity
+@Inheritance(strategy=InheritanceType.SINGLE_TABLE)
+@DiscriminatorValue(value="E")
+public class Employee {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int employeeId;
+	private String firstname,lastName,email;
+	public Employee() {
+		super();
+	}
+	public Employee(String firstname, String lastName, String email) {
+		super();
+		this.firstname = firstname;
+		this.lastName = lastName;
+		this.email = email;
+	}
+	public int getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+}
